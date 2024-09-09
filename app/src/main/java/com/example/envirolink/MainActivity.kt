69238188e122b73d36b2224cf0b50400b63bfc8c
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-09-09 14:22:29 +0700

    feat: add header and city name on Main Page

--- app/src/main/java/com/example/envirolink/MainActivity.kt
+++ app/src/main/java/com/example/envirolink/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.envirolink
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.envirolink.ui.theme.EnviroLinkTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Row(
+                            horizontalArrangement = Arrangement.End,
+                            modifier = Modifier.background(Color(0x20FF0000)).fillMaxWidth().padding(top = 26.dp, start = 20.dp, end = 20.dp)
+                        ) {
+                           PlaceholderCircle()
+                        }
+                        Row(Modifier.background(Color(0x2000FF00)).padding(top = 38.dp, bottom = 70.dp)) {
+                            Text("SLEMAN", textAlign = TextAlign.Center, fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth())
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +57,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EnviroLinkTheme {
-        Greeting("Android")
+fun PlaceholderCircle() {
+    Box(
+        Modifier
+            .clip(CircleShape)
+            .background(Color(0xFFD9D9D9))
+            .width(50.dp)
+            .height(50.dp)) {
     }
 }
