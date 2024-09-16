ab08e3222980bf0d55863aeffc9a10a4a9981cbb
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-09-16 18:45:52 +0700

    feat: add navigation to dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
